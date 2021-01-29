c5acf325b91ea84f6e4109479e28ada38c13ed91
Melissa <wolfaholic@users.noreply.github.com>
2021-01-29 15:57:09 -0800

    Events 1 and 2 complete
    Mouse Enter and Mouse Leave will resize the nav item it hovers over.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+//Event 1 and 2 - The text of the navigation will be resized to 1.5 scale when the mouse is over the nav on mouse enter and back to normal when the mouse leaves.
+
+const resize = document.querySelectorAll('a');
+resize.forEach(element => {element.addEventListener('mouseenter', () => {
+    element.style.transform = "scale(1.5)";
+    element.style.transform = "all 0.3s";
+})})
+
+const exit = document.querySelectorAll('a');
+exit.forEach(element => {element.addEventListener('mouseleave', () => {
+    element.style.transform = "scale(1)";
+    element.style.transform = "all 0.3s";
+})})
+
